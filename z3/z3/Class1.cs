adda9fcf020a6363ae9e57828c15f9661c61c4e0
sandrayakimova <43165224+sandrayakimova@users.noreply.github.com>
2019-07-14 15:17:14 +0300

    Add files via upload

---
+++ z3/z3/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace z3
+{
+    public class Class1
+    {
+        public static string[] OrderStringsByLength(string[] massivstr)
+        {
+            string temp;
+            for (int i = 0; i < massivstr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < massivstr.Length; j++)
+                {
+                    if (massivstr[i].Length > massivstr[j].Length)
+                    {
+                        temp = massivstr[i];
+                        massivstr[i] = massivstr[j];
+                        massivstr[j] = temp;
+                    }
+                }
+            }
+            return massivstr;
+        }
+    }
+}
