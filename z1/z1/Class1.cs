ed1f9b418696d73ce2505446550f192cdef9a9d1
sandrayakimova <43165224+sandrayakimova@users.noreply.github.com>
2019-07-14 14:57:42 +0300

    Add files via upload

---
+++ z1/z1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace z1
+{
+    public class Class1
+    {
+        public static long GetGCD(long a, long b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                { a = a % b; }
+                else
+                { b = b % a; }
+            }
+
+            return a + b;
+        }
+
+    }
+}
