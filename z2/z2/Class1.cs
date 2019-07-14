518ed9a2882f5c266dc932787142f64fdd4ec8e7
sandrayakimova <43165224+sandrayakimova@users.noreply.github.com>
2019-07-14 15:01:35 +0300

    Add files via upload

---
+++ z2/z2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace z2
+{
+    public class Class1
+    {
+        public static int CountOfVowels(string str)
+        {
+            int vowelCount = 0;
+            int k = str.Length;
+            for (int i = 0; i < k; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
